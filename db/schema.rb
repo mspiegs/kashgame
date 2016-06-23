a8cbf8ee71e31665f688c4a6498587bff0d366f1
mspiegs <matt.speigel@gmail.com>
2016-06-22 21:34:55 -0700

    MS - Add game field to rounds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623043011) do
+ActiveRecord::Schema.define(version: 20160623043431) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160623043011) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "course_id"
+    t.string   "game"
   end
 
   create_table "rounds_users", force: :cascade do |t|
