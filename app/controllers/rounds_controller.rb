555ee2b01c4b3c5badb419f8815f5b1398bf80ed
mspiegs <matt.speigel@gmail.com>
2016-06-25 11:27:28 -0700

    MS - JSON response for rounds show

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -2,5 +2,10 @@ class RoundsController < ApplicationController
 
   def show
     @round = Round.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @round }
+    end
   end
 end
