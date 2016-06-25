2a138798e163e709f715cb7de14adb7792de6e55
mspiegs <matt.speigel@gmail.com>
2016-06-24 21:26:51 -0700

    MS - Added length to holes

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,10 +1,3 @@
 module GamesHelper
-  if @round.game = "nassau"
-    nassau
-  end
 
-  private
-
-    def nassau
-    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160624033900) do
+ActiveRecord::Schema.define(version: 20160625042626) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160624033900) do
     t.integer  "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "length"
   end
 
   create_table "players", force: :cascade do |t|
