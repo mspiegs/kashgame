fcd723f455091db1c5682754770bfaf1c1973bcc
Matt Spiegel <matt@expoguru.com>
2016-06-27 16:39:01 -0700

    MS - Added scores relationship to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_and_belongs_to_many :rounds
   has_and_belongs_to_many :games
+  has_many :scores
 end
