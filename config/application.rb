04df0d183bfb85e6aeb953a2a64d69d488f4a456
Matt Spiegel <matt@expoguru.com>
2017-02-13 21:57:26 -0800

    MS - rollback to localhost CORS

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module GolfBet
     config.active_record.raise_in_transactional_callbacks = true
 
     config.action_dispatch.default_headers = {
-        'Access-Control-Allow-Origin' => 'https://still-refuge-37868.herokuapp.com/',
+        'Access-Control-Allow-Origin' => 'http://localhost:8100/',
         'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
       }
   end
