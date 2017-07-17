1cd50a4c34844388640f4c7b3cecbadf9025d2bb
mspiegs <matt.spiegel@gmail.com>
2017-07-16 18:39:00 -0700

    MS - Fix gameholes relationship

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,7 @@ class Game < ActiveRecord::Base
   belongs_to :round
   has_and_belongs_to_many :users
   has_many :holes, through: :gameholes
+  has_many :gameholes
 
   def get_winners
 
--- app/models/hole.rb
+++ app/models/hole.rb
@@ -2,4 +2,5 @@ class Hole < ActiveRecord::Base
   belongs_to :course
   has_many :scores
   has_many :games, through: :gameholes
+  has_many :gameholes
 end
